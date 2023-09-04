2d8e8f4846e4a2b1134fe25c7eeab97d748a47c7
vismaya2002 <vismay2002@gmail.com>
2023-09-04 14:04:44 +0530

    created editing of doctor

--- modules/doctor.py
+++ modules/doctor.py
@@ -21,36 +21,58 @@ def NameEdit(id,connection,cursor):
     newname = input("enter the new name of doctor: \n")
     cursor.execute("update doctor set name='{}' where doctorid={}".format(newname,id))
     connection.commit()
-    print("\n patient name updated successfully \n")
+    print("\n Doctor name updated successfully \n")
 
-def AgeEdit():
+def AgeEdit(id,connection,cursor):
     newage = input("enter the new age of doctor: \n")
+    cursor.execute("update doctor set age={} where doctorid={}".format(newage,id))
+    connection.commit()
+    print("\n Doctor age updated successfully \n")
 
-def deptedit():
-    newdept = input("enter the new department: \n")
+def Deptedit(id,connection,cursor):
+    newdept = int(input("enter the new departmentid : \n"))
+    cursor.execute("update doctor set deptid={} where doctorid={}".format(newdept,id))
+    connection.commit()
+    print("\n Doctor department id updated successfully \n")
 
-def qualifedit():
+def Qualifedit(id,connection,cursor):
     newqualifi = input("enter the new qualifications: \n")
+    cursor.execute("update doctor set qualification='{}' where doctorid={}".format(newqualifi,id))
+    connection.commit()
+    print("\n Doctor qualification updated successfully \n")
 
-def phnedit():
+def Phnedit(id,connection,cursor):
     newphn = input("enter the new phone number of doctor: \n")
+    cursor.execute("update doctor set phnnumber={} where doctorid={}".format(newphn,id))
+    connection.commit()
+    print("\n Doctor phn number updated successfully \n")
 
 
 def EditDoctor(connection,cursor):
     while True:
         print("Edit The Details of Doctor...")
-        id = int(input("enter the id of doctor: \n"))
-        print("1. Enter the edited name of doctor: \n")
-        print("2. Enter the edited age of doctor: \n")
-        print("3. Enter the edited department of doctor: \n")
-        print("4. Enter the edited qualification of doctor: \n")
-        print("5. Enter the edited phone number of doctor: \n")
+        id = int(input("enter the id of doctor \n"))
+        print("1. Enter the edited name of doctor \n")
+        print("2. Enter the edited age of doctor \n")
+        print("3. Enter the edited department of doctor \n")
+        print("4. Enter the edited qualification of doctor \n")
+        print("5. Enter the edited phone number of doctor \n")
         y = int(input("Enter your choice: \n"))
         match y:
             case 1:
                 NameEdit(id,connection,cursor)
             case 2:
+                AgeEdit(id,connection,cursor)
+            case 3:
+                Deptedit(id,connection,cursor)
+            case 4:
+                Qualifedit(id,connection,cursor)
+            case 5:
+                Phnedit(id,connection,cursor)
+            case 6:
                 break
+            case default:
+                print("You Entered The Wrong Choice.")
 
 def Doctor(connection,cursor):
     while True:
@@ -64,9 +86,7 @@ def Doctor(connection,cursor):
         match x:
             case 1:
                 InsertDoctor(connection,cursor)
-
             case 2:
-                EditDoctor()
-                
-            case 5:
+                EditDoctor(connection,cursor)
+            case 3:
                 break
